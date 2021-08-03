4db7ccd6c74307f71829b3e5ab7aa4fc2f4e7e06
Lucian <lucian.miller185@gmail.com>
2021-08-03 11:48:36 -0700

    It will add prototype to switch players and redue HTML

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ function Game() {
   this.players = [];
   this.currentScore = 0;
   this.currentPlayer = 0;
+  this.maxPlayers = 2;
 }
 
 function Player(name) {
@@ -14,8 +15,19 @@ Game.prototype.addPlayers = function(...playerObjects) {
   this.players.push(...playerObjects);
 }
 
-function diceRoller(min,max) {
-  return Math.floor(Math.random() * (max - min) + min);
+Game.prototype.switchPlayers = function() {
+  if (this.currentPlayer + 1 != this.maxPlayers) {
+    this.currentPlayer += 1;
+  } else if (this.currentPlayer + 1 == this.maxPlayers) {
+    this.currentPlayer = 0;
+  }
+
+
+  console.log("Current player index: ", this.currentPlayer, "Current Player: ", this.players[this.currentPlayer]);
+}
+
+function rollD6() {
+  return Math.floor(Math.random() * (7 - 1) + 1);
 }
 
 let newGame = new Game();
@@ -24,6 +36,8 @@ let player1 = new Player("Fred");
 let player2 = new Player("Bob");
 
 newGame.addPlayers(player1, player2);
+newGame.switchPlayers();
+newGame.switchPlayers();
+newGame.switchPlayers();
 
 console.log(newGame.players);
-console.log(diceRoller(1,6))
