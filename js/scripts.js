e698a02cf388fce205428b5928081eaaecd1f06b
Lucian <lucian.miller185@gmail.com>
2021-08-05 11:38:48 -0700

    It will calulate score and display info about player on screen

--- js/scripts.js
+++ js/scripts.js
@@ -21,33 +21,46 @@ Game.prototype.switchPlayers = function() {
   } else if (this.currentPlayer + 1 == this.maxPlayers) {
     this.currentPlayer = 0;
   }
-
-
-  console.log("Current player index: ", this.currentPlayer, "Current Player: ", this.players[this.currentPlayer]);
+  console.log("current player: " + this.currentPlayer);
 }
 
 function rollD6() {
   return Math.floor(Math.random() * (7 - 1) + 1);
 }
 
-// let player2 = new Player("Bob");
-
-// newGame.addPlayers(player1, player2);
-// newGame.switchPlayers();
-// newGame.switchPlayers();
-// newGame.switchPlayers();
-
-// console.log(newGame.players);
+Game.prototype.calculateScore = function(points) {
+  if (points !== 1) {
+    this.currentScore += points;
+  } else if (points === 1) {
+    this.currentScore = 0;
+    this.switchPlayers();
+  }
+  
+  console.log("points: " + points);
+  console.log("current score: " + this.currentScore);
+}
 
 // User Interface Logic ----------
+displayGameInfo = function(game) {
+  $("#current-player").show();
+  $("#player-name").text(`Current Player: ${game.players[game.currentPlayer].name}`);
+  $("#current-score").text(`Current score: ${game.currentScore}`);
+  $("#total-score").text(`Total Score: ${game.players[game.currentPlayer].score}`);
+}
+
+
 $(document).ready(function() {
   $("#start-game").on("click", function() {
     $("form#game-info").hide();
     $("#start-game").hide();
     $("#buttons").show();
+    displayGameInfo(newGame);
   });
   $("#roll").on("click", function() {
-    $("#output").text(rollD6());
+    let roll = rollD6();
+    newGame.calculateScore(roll);
+    $("#output").text(roll);
+    displayGameInfo(newGame);
   });
   $("#hold").on("click", function() {
     // Will hold turn
