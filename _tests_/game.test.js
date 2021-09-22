45753a15ef563fa00b4fb2b25e5e125c95bb3470
Lucian Miller <lucian.miller185@gmail.com>
2021-09-22 09:42:52 -0700

    add test for addPlayers prototype

--- _tests_/game.test.js
+++ _tests_/game.test.js
@@ -2,14 +2,30 @@ import Game from './../src/game.js';
 import Player from './../src/player.js';
 
 describe('Game', () => {
+  let newGame;
+
+  beforeEach(() => {
+    newGame = new Game();
+  });
 
   test('should correctly create new game object with empty variables', () => {
-    const newGame = new Game();
     expect(newGame.players).toEqual([]);
     expect(newGame.currentScore).toEqual(0);
     expect(newGame.currentPlayerIndex).toEqual(0);
     expect(newGame.maxPlayers).toEqual(2);
   });
 
-  test('should ')
+  test('should push 1 Player object into Game objects players array', () => {
+    const player1 = new Player("Sam");
+    newGame.addPlayers(player1);
+    expect(newGame.players[0]).toEqual(player1);
+  });
+
+  test('should push multiple Player objects into Game objects players array', () => {
+    const player1 = new Player("Sam");
+    const player2 = new Player ("Sally");
+    newGame.addPlayers(player1, player2);
+    expect(newGame.players[0]).toEqual(player1);
+    expect(newGame.players[1]).toEqual(player2);
+  });
 });
